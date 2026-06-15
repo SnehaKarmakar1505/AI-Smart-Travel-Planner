c5b1144e4fc10813fc673dbf9a759443c9e3f4c4
SnehaKarmakar1505 <snehaofficial1505@gmail.com>
2026-06-15 22:38:53 +0530

    Initial commit - AI Smart Travel Planner

---
+++ app.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, request
+from google import genai
+
+app = Flask(__name__)
+
+client = genai.Client(
+    api_key="YOUR_API_KEY_HERE"
+)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/planner")
+def planner():
+    return render_template("index.html")
+
+
+@app.route("/generate", methods=["POST"])
+def generate():
+
+    destination = request.form["destination"]
+    days = request.form["days"]
+    budget = request.form["budget"]
+    travel_type = request.form["travel_type"]
+
+    prompt = f"""
+    Create a professional travel itinerary.
+
+    Destination: {destination}
+    Number of Days: {days}
+    Budget: {budget}
+    Travel Type: {travel_type}
+
+    Include:
+    1. Day-wise itinerary
+    2. Budget breakdown
+    3. Best places to visit
+    4. Local food recommendations
+    5. Packing suggestions
+
+    Format nicely.
+    """
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash-lite",
+        contents=prompt
+    )
+
+    result = response.text
+
+    return render_template(
+        "index.html",
+        itinerary=result
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
